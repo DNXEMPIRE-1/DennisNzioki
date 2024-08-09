c2d4ccf0665daf1b45bd48540f6403bd9428555b
Dennis Nzioki <dennisnzioki019@gmail.com>
2024-08-09 15:01:54 +0300

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./style.css");
+const Application_1 = __importDefault(require("./Application/Application"));
+const app = new Application_1.default();
