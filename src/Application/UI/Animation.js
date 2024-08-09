befb5ebe629e2a02fedccb5b5b20b4c598c42775
Dennis Nzioki <dennisnzioki019@gmail.com>
2024-08-09 15:04:57 +0300

    Create Animation.js

---
+++ src/Application/UI/Animation.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Easing = void 0;
+exports.Easing = {
+    sHard: [0.95, 0, 0.5, 1],
+    sMedium: [0.8, 0, 0.2, 1],
+    sSoft: [0.6, 0, 0.4, 1],
+    expIn: [0.9, 0.05, 1, 0.3],
+    expOut: [0.05, 0.7, 0.1, 1],
+    expInOut: [0.9, 0.05, 0.1, 1],
+};
